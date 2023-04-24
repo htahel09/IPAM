16baf4cd0085da59e34d37d2fcd07a72981c4221
htahel09 <128191889+htahel09@users.noreply.github.com>
2023-04-23 23:08:58 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from image_processing import IPAM 
-from color_modifier import IPAM
+from image_processing import IPAM1 
+from color_modifier import IPAM2
 import matplotlib.pyplot as plt
 import numpy as np
